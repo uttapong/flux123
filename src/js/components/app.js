f8e8435d622a1783b10fb38645f6b126c93449dc
Uttapong Ruangrit <uttapong@gmail.com>
2015-04-24 18:45:35 +0700

    first commit

---
+++ src/js/components/app.js
@@ -0,0 +1,19 @@
+/** @jsx React.DOM */
+var React = require('react');
+var AppActions = require('../actions/app-actions.js');
+var Catalog = require('../components/app-catalog.js');
+var Cart = require('../components/app-cart.js');
+var APP =
+  React.createClass({
+    handleClick:function(){
+      AppActions.addItem('this is the item');
+    },
+    render:function(){
+      return <div><h1>lets shop</h1>
+        <Catalog />
+        <h1>Cart</h1>
+        <Cart />
+        </div>
+    }
+  });
+module.exports = APP;
